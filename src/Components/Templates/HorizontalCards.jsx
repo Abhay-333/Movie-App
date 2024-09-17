37f88e4609a294f68bbd2b383a6798f70ad10b54
Abhay <abhayd0331@gmail.com>
2024-09-17 15:47:36 +0530

    error 5 resolve

--- src/Components/Templates/HorizontalCards.jsx
+++ src/Components/Templates/HorizontalCards.jsx
@@ -9,34 +9,27 @@ const HorizontalCards = ({ data }) => {
         {data && data.map((card, index) => (
           <Link
             to={`/${card.media_type}/details/${card.id}`}
-            key={index}
-            style={
-              card.profile_path ||
-              card.backdrop_path ||
-              card.profile_path ||
-              card.poster_path
-                ? {
-                    background: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.5), rgba(0,0,0,.7)), url(https://image.tmdb.org/t/p/original/${
-                      card.backdrop_path || card.poster_path
-                    })`,
-                    backgroundPosition: "center",
-                    backgroundSize: "cover",
-                    backgroundRepeat: "no-repeat",
-                  }
-                : noImg
-            }
+            key={card.id}
+            style={{
+              background: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.5), rgba(0,0,0,.7)), url(${
+                card.backdrop_path || card.poster_path
+                  ? `https://image.tmdb.org/t/p/original/${card.backdrop_path || card.poster_path}`
+                  : noImg
+              })`,
+              backgroundPosition: "center",
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+            }}
+
             className="card w-[20%] h-full border-[1px] border-gray-400 flex-shrink-0 rounded-lg flex items-end justify-center"
           >
             <div className="cardDetails flex flex-col p-4">
               <h1 className="text-xl font-semibold my-2">
-                {card.original_name ||
-                  card.title ||
-                  card.name ||
-                  card.original_title}
+              {card?.original_name || card?.title || card?.name || card?.original_title}
               </h1>
 
               <p className="text-sm">
-                {card.overview.slice(0, 50)}{" "}
+              {card?.overview ? card.overview.slice(0, 50) : ''}
                 <Link
                   to={`/${card.media_type}/details/${card.id}`}
                   className="text-blue-400 font-semibold "
