09de229567349db02f0ada470ed9e2adffb24b8e
Abhay <abhayd0331@gmail.com>
2024-09-17 15:38:45 +0530

    error 4 resolve

--- src/Components/Templates/HorizontalCards.jsx
+++ src/Components/Templates/HorizontalCards.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import noImg from '../../Resources/no-img.webp'
+import noImg from "../../Resources/no-img.webp";
 
 const HorizontalCards = ({ data }) => {
   return (
     <div className="min-h-[40vh] w-full px-5 py-3 overflow-y-auto">
       <div className="cards h-[20rem] w-full gap-5 flex">
-        {data.map((card, index) => (
+        {data && data.map((card, index) => (
           <Link
             to={`/${card.media_type}/details/${card.id}`}
             key={index}
-            style={ card.profile_path || card.backdrop_path || card.profile_path || card.poster_path ?{ 
-              background: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.5), rgba(0,0,0,.7)), url(https://image.tmdb.org/t/p/original/${
-                card.backdrop_path ||  card.poster_path
-              })`,
-              backgroundPosition: "center",
-              backgroundSize: "cover",
-              backgroundRepeat: "no-repeat",
-            } : noImg}
+            style={
+              card.profile_path ||
+              card.backdrop_path ||
+              card.profile_path ||
+              card.poster_path
+                ? {
+                    background: `linear-gradient(rgba(0,0,0,.2), rgba(0,0,0,.5), rgba(0,0,0,.7)), url(https://image.tmdb.org/t/p/original/${
+                      card.backdrop_path || card.poster_path
+                    })`,
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                  }
+                : noImg
+            }
             className="card w-[20%] h-full border-[1px] border-gray-400 flex-shrink-0 rounded-lg flex items-end justify-center"
           >
             <div className="cardDetails flex flex-col p-4">
@@ -30,7 +37,12 @@ const HorizontalCards = ({ data }) => {
 
               <p className="text-sm">
                 {card.overview.slice(0, 50)}{" "}
-                <Link to={`/${card.media_type}/details/${card.id}`} className="text-blue-400 font-semibold ">...more</Link>
+                <Link
+                  to={`/${card.media_type}/details/${card.id}`}
+                  className="text-blue-400 font-semibold "
+                >
+                  ...more
+                </Link>
               </p>
             </div>
           </Link>
